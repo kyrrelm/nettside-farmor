8f1fa810bb04226bb4a031046ebd34f5eb7da9ac
Kyrrre Moe <kyrre.l.moe@gmail.com>
2020-10-04 21:23:39 +0200

    Slide.tsx

--- src/album/Project.tsx
+++ src/album/Project.tsx
@@ -1,7 +1,6 @@
 import React, { CSSProperties, useLayoutEffect, useState } from "react";
 import AutoRotatingCarousel from "../carousel/AutoRotatingCarousel";
 import Slide from "../carousel/Slide";
-import {blue, green, red} from "@material-ui/core/colors";
 
 const COLUMNS = 3;
 
@@ -178,33 +177,15 @@ function AutoRotatingCarouselModal({
                 mobile={isMobile}
                 style={{ position: "absolute" }}
             >
-                <Slide
-                    media={
-                        <img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />
-                    }
-                    mediaBackgroundStyle={{ backgroundColor: red[400] }}
-                    style={{ backgroundColor: red[600] }}
-                    title="This is a very cool feature"
-                    subtitle="Just using this will blow your mind."
-                />
-                <Slide
-                    media={
-                        <img src="http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png" />
-                    }
-                    mediaBackgroundStyle={{ backgroundColor: blue[400] }}
-                    style={{ backgroundColor: blue[600] }}
-                    title="Ever wanted to be popular?"
-                    subtitle="Well just mix two colors and your are good to go!"
-                />
-                <Slide
-                    media={
-                        <img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />
-                    }
-                    mediaBackgroundStyle={{ backgroundColor: green[400] }}
-                    style={{ backgroundColor: green[600] }}
-                    title="May the force be with you"
-                    subtitle="The Force is a metaphysical and ubiquitous power in the Star Wars fictional universe."
-                />
+                <Slide>
+                    <img src="http://www.icons101.com/icon_png/size_256/id_79394/youtube.png" />
+                </Slide>
+                <Slide>
+                    <img src="http://www.icons101.com/icon_png/size_256/id_80975/GoogleInbox.png" />
+                </Slide>
+                <Slide>
+                    <img src="http://www.icons101.com/icon_png/size_256/id_76704/Google_Settings.png" />
+                </Slide>
             </AutoRotatingCarousel>
         </div>
     );
---
+++ src/carousel/Slide.tsx
@@ -0,0 +1,18 @@
+import React, {ReactNode} from "react";
+import blue from "@material-ui/core/colors/blue";
+
+const styles = {
+    style: {
+        backgroundColor: blue[500],
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+};
+
+export default function Slide({children}: {children: ReactNode | ReactNode[]}) {
+    return <div style={styles.style}>{children}</div>;
+}
+
