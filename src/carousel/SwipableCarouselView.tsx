4ad1fde400fae6b4a46e194326e4a3d4949d6ffa
Kyrrre Moe <kyrre.l.moe@gmail.com>
2020-10-14 15:50:52 +0200

    more ts

--- src/carousel/SwipableCarouselView.tsx
+++ src/carousel/SwipableCarouselView.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+// @ts-ignore
 import virtualize from 'react-swipeable-views-utils/lib/virtualize'
+// @ts-ignore
 import bindKeyboard from 'react-swipeable-views-utils/lib/bindKeyboard'
 import SwipeableViews from 'react-swipeable-views'
 import { modulo } from './util'
 
 const VirtualizeSwipeViews = bindKeyboard(virtualize(SwipeableViews))
 
-const carouselSlideRenderer = (children) =>
-  ({index, key}) => React.cloneElement(children[modulo(index, children.length)], {key})
+const carouselSlideRenderer = (children: any) =>
+  ({index, key}: any) => React.cloneElement(children[modulo(index, children.length)], {key})
 
-export default function Carousel ({children, ...other}) {
+export default function SwipableCarouselView ({children, ...other}: any) {
   const slideRenderer = carouselSlideRenderer(children)
   return (
     <VirtualizeSwipeViews
